cc4ae1a71c536fe1a8774237141d192edf492dd9
rashith55 <49812767+rashith55@users.noreply.github.com>
2020-05-24 01:22:16 +1000

    Update main.php
    Added user login tracing, store in google  data store

--- Website/main.php
+++ Website/main.php
@@ -7,6 +7,50 @@
 
   <?php
   session_start();
+  include( "Google/Client.php" );
+  include( "Google/Service/Storage.php" );
+  
+  
+
+ 
+  $file_content = "user" . $_SESSION['name']. "logged in at:" . $date;
+  
+  $handle = fopen('gs://login-logs/logs.txt','a');
+  fwrite($handle,$file_content);
+  fclose($handle);
+
+  
+/*   $client = new Google_Client();
+  putenv('GOOGLE_APPLICATION_CREDENTIALS=CloudCompA2-aea743ad31f4.json');
+  $client->useApplicationDefaultCredentials();
+  
+  
+  $storageService = new Google_Service_Storage( $client );
+
+  
+  
+  try 
+  {
+
+	$postbody = array( 
+			'name' => $file_name, 
+			//'data' => $file_content,
+			'uploadType' => "media"
+			);
+
+	$gsso = new Google_Service_Storage_StorageObject();
+	$gsso->setName( $file_name );
+
+	$result = $storageService->objects->insert( $bucket, $gsso, $postbody );
+
+	print_r($result);
+	 
+  }       
+  catch (Exception $e)
+  {
+	print $e->getMessage();
+  }
+   */
   ?>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript">
  </script>
@@ -18,6 +62,9 @@
     var port= 33828;
     var user="khnpnwap";
     var pass="OV4PZj_k9oBG";
+	
+	
+	
 
   function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
